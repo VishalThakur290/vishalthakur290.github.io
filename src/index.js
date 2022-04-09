230dec2c40e72af194295041c0e81067cee805c2
Vishal Thakur <vkumar250999@gmail.com>
2022-04-10 01:22:47 +0530

    updated file

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { Provider } from 'react-redux';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/weather-app">
         <App />
       </BrowserRouter>
     </Provider>
